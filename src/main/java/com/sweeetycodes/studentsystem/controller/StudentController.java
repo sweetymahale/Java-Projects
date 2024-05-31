c5e77686b8d9eddfb15e3296293ed355e5876657
Sweety Mahale <mahalesweety18@gmail.com>
2024-05-31 10:44:11 -0500

    Added React app for UI

--- src/main/java/com/sweeetycodes/studentsystem/controller/StudentController.java
+++ src/main/java/com/sweeetycodes/studentsystem/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.sweeetycodes.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/student")
 public class StudentController {
@@ -15,5 +17,9 @@ public class StudentController {
      public String add(@RequestBody Student student){
          studentService.saveStudent(student);
          return "New student is added";
+    }
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+         return studentService.getAllStudents();
     }
 }
